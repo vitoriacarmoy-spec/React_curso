d14599290549a274a321e3ee0bbd2ab6726cd901
vitoriacarmoy-spec <vitoria.carmoy@gmail.com>
2026-08-07 00:47:52 -0300

    min14
    erro com a linkagem

---
+++ my-app/src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+
+import './style.css'
+
+function Home() {
+ 
+
+  return (
+  <div>
+    <h1>
+      ola react
+    </h1>
+  </div>
+  )
+}
+
+export default Home
--- my-app/src/pages/main.tsx
+++ my-app/src/pages/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import home from './pages/home/home'
-
+import Home from './pages/Home'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
    
-    <home />
+    <Home/>
   </StrictMode>,
 )
