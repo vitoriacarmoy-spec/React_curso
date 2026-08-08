73464243a92e201598374c27a90ac4c64bf6b973
vitoriacarmoy-spec <vitoria.carmoy@gmail.com>
2026-08-07 21:40:43 -0300

    hum

--- my-app/src/pages/main.tsx
+++ my-app/src/pages/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './pages/Home/index.css'
 import Home from './pages/Home'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
